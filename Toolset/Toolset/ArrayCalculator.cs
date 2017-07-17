ee0aa4f29dbcd973ad365afcb1b6df2438f305a2
cyy132 <cyy132@ftr.com>
2017-07-17 13:27:50 -0400

    Toolset Build

---
+++ Toolset/Toolset/ArrayCalculator.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rk3.Toolset
+{
+    public class ArrayCalculator<T>
+    {
+        public T[] arr { get; private set; }
+
+        public ArrayCalculator(T[] arr)
+        {
+            if (arr == null)
+                throw new ArgumentNullException("arr");
+            if (arr.Length == 0)
+                throw new ArgumentException("Invalid array length of zero.");
+
+            this.arr = arr;
+        }
+
+        public int GetMedian()
+        {
+            if (typeof(T) == typeof(int))
+                return medianOfMedians(this.arr as int[], (int)Math.Floor((double)this.arr.Length / 2));
+            else
+                throw new NotSupportedException(string.Format("Cannot get median of array that is of type {0}.", typeof(T)));
+        }
+
+        private int medianOfMedians(int[] a, int index)
+        {
+            int iSubGrpLen = 5;
+            var iSplitCount = Math.Ceiling((double)a.Length / iSubGrpLen);
+            int[][] arrSubsets = new int[(int)iSplitCount][];
+            int iMedian;
+            int iPosition = 0;
+
+            for (int i = 0; i < iSplitCount; i++)
+            {
+                //if there are less than 5 remaining values in subset, adjust length
+                if (i == iSplitCount - 1 && a.Length % 5 != 0)
+                    iSubGrpLen = (int)(a.Length % 5);
+
+                arrSubsets[i] = new int[iSubGrpLen];
+
+                for (int j = 0; j < iSubGrpLen; j++)
+                {
+                    arrSubsets[i][j] = a[j + iPosition];
+                }
+                iPosition += iSubGrpLen;
+            }
+
+            //Get medians of all subgroups into an array
+            int[] arrSubGrpMedians = new int[(int)iSplitCount];
+
+            for (var i = 0; i < arrSubsets.Length; i++)
+            {
+                Array.Sort(arrSubsets[i]);
+                arrSubGrpMedians[i] = arrSubsets[i][arrSubsets[i].Length / 2];
+            }
+
+            //If more than 5 values in array, then recurse
+            if (arrSubGrpMedians.Length <= 5)
+                iMedian = arrSubGrpMedians.OrderBy(x => x).ElementAt(arrSubGrpMedians.Length / 2);
+            else
+                iMedian = medianOfMedians(arrSubGrpMedians, arrSubGrpMedians.Length / 2);
+
+            //Separate lesser values to the left of the median, and greater to the right
+            int[] low = a.Where(x => x < iMedian).ToArray();
+            int[] high = a.Where(x => x > iMedian).ToArray();
+
+            var k = low.Length;
+
+            //Recursively narrow in on value based on provided index
+            if (index < k)
+                return medianOfMedians(low, index);
+            else if (index > k)
+                return medianOfMedians(high, index - k - 1);
+            else
+                return iMedian;
+        }
+    }
+}
---
+++ Toolset/ToolsetTest/ArrayCalculatorTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rk3.Toolset;
+
+namespace ToolsetTest
+{
+    [TestClass]
+    public class ArrayCalculatorTest
+    {
+        [TestMethod]
+        public void Median_WithValidArrayType_ReturnsMedianValue()
+        {
+            int[][] arrays = new int[5][];
+            Random randomSize = new Random();
+            for (int i = 0; i < arrays.Length; i++)
+            {
+                arrays[i] = generateRandomIntArray(1, 1000, randomSize.Next(1, 200));
+                //int selectIndex = randomSize.Next(1, arrays[i].Length);
+                int selectIndex = arrays[i].Length / 2;
+                ArrayCalculator<int> arrCalc = new ArrayCalculator<int>(arrays[i]);
+                var med = arrCalc.GetMedian();
+                Assert.IsTrue(med > arrays[i].Min() && med < arrays[i].Max());
+                Assert.AreEqual(arrays[i].OrderBy(x => x).ElementAt(selectIndex), med, 
+                    string.Format("\nIndex: {0}\n{1}",selectIndex, string.Join(System.Environment.NewLine, arrays[i].OrderBy(x => x))));
+            }
+        }
+
+        [TestMethod]
+        public void Median_WithInvalidArrayType_ThrowsException()
+        {
+            try
+            {
+                string[] arrString = new string[]{"t1","t2"};
+                ArrayCalculator<string> arrCalc = new ArrayCalculator<string>(arrString);
+                arrCalc.GetMedian();
+            }
+            catch (NotSupportedException ex)
+            {
+                StringAssert.Contains(ex.Message, "Cannot get median of array that is of type");
+                return;
+            }
+            catch (Exception)
+            {
+                Assert.Fail("Incorrect exception type was thrown.");
+            }
+            Assert.Fail("No exception was thrown");
+        }
+
+        private int[] generateRandomIntArray(int MinVal, int MaxVal, int Size)
+        {
+            int[] ret = new int[Size];
+
+            Random randNum = new Random();
+
+            for (int i = 0; i < ret.Length; i++)
+            {
+                ret[i] = randNum.Next(MinVal, MaxVal);
+            }
+
+            return ret;
+        }
+    }
+}
